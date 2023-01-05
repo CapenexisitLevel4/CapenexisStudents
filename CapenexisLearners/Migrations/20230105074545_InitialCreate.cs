9b281138253ef4cd7e1818f1bf67cdc35f6b8c27
CapenexisitLevel4 <[capenexisitlevel4.2022@gmail.com]>
2023-01-05 11:51:17 +0200

    Add project files.

---
+++ CapenexisLearners/Migrations/20230105074545_InitialCreate.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CapenexisLearners.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Learner",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    SurName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    NationalIdentityNumber = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Course = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Price = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Duration = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Learner", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Learner");
+        }
+    }
+}
---
+++ CapenexisLearners/Models/Learner.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CapenexisLearners.Models;
+
+public class Learner
+{
+    public int Id { get; set; }
+    public string SurName { get; set; }
+   
+    public string FirstName{ get; set; }
+    public string NationalIdentityNumber { get; set; }
+    public string Course { get; set; }
+    public decimal Price { get; set; }
+    public string Duration { get; set; }
+}
